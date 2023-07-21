3c9fd4096185c5e320dfd59818bd5eae3da7bd34
Mirosław Borowiecki <mborowiecki@graylight-imaging.com>
2023-07-22 00:00:34 +0200

    Move the write (persistence) model to keep it together with other infrastructure details.

--- Scoreboard/Scoreboard.cs
+++ Scoreboard/Scoreboard.cs
@@ -55,29 +55,3 @@ public record MatchScore(
     int HomeTeamScore, 
     int AwayTeamScore
     );
-
-internal class MatchScoreModel
-{
-    protected MatchScoreModel() { }
-
-    public Guid Id { get; set; }
-    public string HomeTeam { get; set; } = null!;
-    public string AwayTeam { get; set; } = null!;
-    public int HomeScore { get; set; }
-    public int AwayScore { get; set; }
-
-    public DateTime? Added { get; set; }
-
-    public static MatchScore ToMatchScore(MatchScoreModel model)
-        => new(model.Id, model.HomeTeam!, model.AwayTeam!, model.HomeScore, model.AwayScore);
-
-    public static MatchScoreModel Create(string homeTeam, string awayTeam) => new()
-    {
-        Id = Guid.NewGuid(),
-        HomeTeam = homeTeam,
-        AwayTeam = awayTeam,
-        HomeScore = 0,
-        AwayScore = 0,
-        Added = DateTime.UtcNow
-    };
-}
