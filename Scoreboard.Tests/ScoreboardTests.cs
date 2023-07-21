c958f0d782e10d9b3ddf4586eacfd0eface1be74
Mirosław Borowiecki <mborowiecki@graylight-imaging.com>
2023-07-21 10:09:04 +0200

    First parameterised test for StartNewMatch argument validation.

--- Scoreboard.Tests/ScoreboardTests.cs
+++ Scoreboard.Tests/ScoreboardTests.cs
@@ -4,7 +4,19 @@ namespace Scoreboard.Tests;
 public class ScoreboardTests
 {
     [TestMethod]
-    public void TestMethod1()
+    [DataRow("Mexico", null, "awayTeam", DisplayName = "AwayTeamNull")]
+    [DataRow("Mexico", "", "awayTeam", DisplayName = "AwayTeamEmpty")]
+    [DataRow("Mexico", "\n\r\t ", "awayTeam", DisplayName = "AwayTeamAllWhitespace")]
+    [DataRow(null, "Canada", "homeTeam", DisplayName = "HomeTeamNull")]
+    [DataRow("", "Canada", "homeTeam", DisplayName = "HomeTeamEmpty")]
+    [DataRow("\n\r\t ", "Canada", "homeTeam", DisplayName = "HomeTeamAllWhitespace")]
+    public void StartNewMatch_Throws_WhenHomeOrAwayTeamAreNotProvided(
+        string homeTeam, string awayTeam, string fieldName)
     {
+        Scoreboard scoreboard = new();
+
+        var ex = Assert.ThrowsException<ArgumentException>(
+            () => scoreboard.StartNewMatch(homeTeam, awayTeam));
+        StringAssert.Contains(ex.Message, fieldName);
     }
 }
--- Scoreboard/Scoreboard.cs
+++ Scoreboard/Scoreboard.cs
@@ -2,5 +2,9 @@
 
 public class Scoreboard
 {
-
+    public void StartNewMatch(string homeTeam, string awayTeam)
+    {
+        if (string.IsNullOrWhiteSpace(homeTeam)) throw new ArgumentException(nameof(homeTeam));
+        if (string.IsNullOrWhiteSpace(awayTeam)) throw new ArgumentException(nameof(awayTeam));
+    }
 }
